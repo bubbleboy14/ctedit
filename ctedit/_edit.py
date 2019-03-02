60f7dff34ef898f3e561491fdfde666cae20bcfd
bubbleboy14 <mario.balibrera@gmail.com>
2019-03-01 17:28:48 -0800

    /_edit request handler: support style action

--- ctedit/_edit.py
+++ ctedit/_edit.py
@@ -1,8 +1,19 @@
 from cantools.web import respond, succeed, fail, cgi_get
-from model import db, PageEdit
+from model import db, PageEdit, Style
 
 def response():
-	data = cgi_get("data")
+	action = cgi_get("action", choices=["edit", "style"])
+	data = cgi_get("data", required=False)
+	if action == "style":
+		style = Style.query().get()
+		if not style:
+			style = Style()
+			style.put() # for key
+		if data:
+			for key, val in data.items():
+				setattr(style, key, val)
+			style.put()
+		succeed(style.data())
 	if "key" in data:
 		entity = db.get(data["key"])
 	else:
