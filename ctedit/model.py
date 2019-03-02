2d04247b15ae3cac6a11e03f45194a5278d7164d
bubbleboy14 <mario.balibrera@gmail.com>
2019-03-01 17:27:50 -0800

    model.Style: font, color, background, rules

--- ctedit/model.py
+++ ctedit/model.py
@@ -4,3 +4,9 @@ class PageEdit(db.TimeStampedBase):
 	path = db.String()
 	content = db.Text()
 	node_id = db.String()
+
+class Style(db.TimeStampedBase):
+	font = db.String()
+	color = db.String()
+	background = db.String()
+	rules = db.Text()
